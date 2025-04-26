d730b1e9ddb50bcf05ad24d190a7a32d4365ceeb
theotime2005 <theotime.berthod@epitech.digital>
2025-04-26 13:30:17 +0200

    feat: add logic to not replace if it's not necessary

--- src/index.js
+++ src/index.js
@@ -56,25 +56,25 @@ async function selectLanguages() {
   return inquirer.prompt(questions);
 }
 
-async function translateObject(obj, fromLang, toLang) {
-  const translated = {};
-
+async function translateObject({obj, translated={}, fromLang, toLang, replace=false}) {
   for (const [key, value] of Object.entries(obj)) {
     if (typeof value === "object" && value !== null) {
-      translated[key] = await translateObject(value, fromLang, toLang);
+      translated[key] = await translateObject({obj: value, translated: translated[key] || {}, fromLang, toLang, replace});
     } else if (typeof value === "string") {
-      try {
-        const result = await translate(value, {
-          from: fromLang,
-          to: toLang,
-        });
-        translated[key] = result.text;
-        console.log(
-          chalk.green(`✓ [${fromLang} → ${toLang}] ${value} → ${result.text}`)
-        );
-      } catch (error) {
-        console.error(chalk.red(`✗ Failed to translate: ${value}`));
-        translated[key] = value;
+      if (!translated[key] || replace) {
+        try {
+          const result = await translate(value, {
+            from: fromLang,
+            to: toLang,
+          });
+          translated[key] = result.text;
+          console.log(
+              chalk.green(`✓ [${fromLang} → ${toLang}] ${value} → ${result.text}`)
+          );
+        } catch (error) {
+          console.error(chalk.red(`✗ Failed to translate: ${value}`));
+          translated[key] = value;
+        }
       }
     } else {
       translated[key] = value;
